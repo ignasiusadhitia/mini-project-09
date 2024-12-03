31881ef3ca688e2393afcddf984301097939b3d7
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-03 20:03:19 +0700

    feat(typography-commons-component): add as props to replace default Tag

--- src/components/commons/Typography.jsx
+++ src/components/commons/Typography.jsx
@@ -2,57 +2,41 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-const Typography = ({ variant, color, className, children }) => {
+const Typography = ({ as: Tag, variant, color, className, children }) => {
   const variants = {
-    header: {
-      tag: 'h1',
-      classes: 'font-bold text-100 leading-[1.1] tracking-[0]',
-    },
-    subHeading: {
-      tag: 'h2',
-      classes: 'font-bold text-44 leading-auto tracking-[0]',
-    },
-    largeText: {
-      tag: 'p',
-      classes: 'font-bold text-350 leading-[2.12] tracking-[0]',
-    },
-    footerText: {
-      tag: 'p',
-      classes: 'font-normal text-24 leading-auto tracking-[0]',
-    },
-    footerTitle: {
-      tag: 'h3',
-      classes: 'font-bold text-24 leading-auto tracking-[0]',
-    },
-    blogCardTitle: {
-      tag: 'h3',
-      classes: 'font-bold text-24 leading-auto tracking-[-2]',
-    },
-    blogCardText: {
-      tag: 'p',
-      classes: 'font-normal text-17 leading-auto tracking-[0]',
-    },
-    blogCardReadMore: {
-      tag: 'a',
-      classes: 'font-bold text-16 leading-auto tracking-[0]',
-    },
-    titleTrust: {
-      tag: 'h1',
-      classes: 'font-bold text-60 leading-auto tracking-[0]',
-    },
-    trustText: {
-      tag: 'p',
-      classes: 'font-normal text-28 leading-auto tracking-[0]',
-    },
+    header: 'font-bold text-100 leading-[1.1] tracking-[0]',
+    subHeading: 'font-bold text-44 leading-auto tracking-[0]',
+    largeText: 'font-bold text-350 leading-[2.12] tracking-[0]',
+    footerText: 'font-normal text-24 leading-auto tracking-[0]',
+    footerTitle: 'font-bold text-24 leading-auto tracking-[0]',
+    blogCardTitle: 'font-bold text-24 leading-auto tracking-[-2]',
+    blogCardText: 'font-normal text-17 leading-auto tracking-[0]',
+    blogCardReadMore: 'font-bold text-16 leading-auto tracking-[0]',
+    titleTrust: 'font-bold text-60 leading-auto tracking-[0]',
+    trustText: 'font-normal text-28 leading-auto tracking-[0]',
   };
 
-  const { tag: Tag, classes } = variants[variant] || {};
-  const baseClasses = `${classes} ${color || ''} ${className || ''}`;
+  const defaultTags = {
+    header: 'h1',
+    subHeading: 'h2',
+    largeText: 'p',
+    footerText: 'p',
+    footerTitle: 'h3',
+    blogCardTitle: 'h3',
+    blogCardText: 'p',
+    blogCardReadMore: 'a',
+    titleTrust: 'h1',
+    trustText: 'p',
+  };
 
-  return Tag ? <Tag className={baseClasses}>{children}</Tag> : null;
+  const ComponentTag = Tag || defaultTags[variant] || 'div';
+  const baseClasses = `${variants[variant] || ''} ${color || ''} ${className || ''}`;
+
+  return <ComponentTag className={baseClasses}>{children}</ComponentTag>;
 };
 
 Typography.propTypes = {
+  as: PropTypes.string,
   variant: PropTypes.oneOf([
     'header',
     'subHeading',

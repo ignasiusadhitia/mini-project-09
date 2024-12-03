956da19d792abeafd49ed4461200bbb3410b92d5
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-03 20:28:25 +0700

    feat: add redux persist and encryption

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import { store } from '@store/index';
+import { persistor, store } from '@store/index';
 
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </PersistGate>
+  </Provider>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,41 @@
+import { combineReducers } from 'redux';
+import { persistStore } from 'redux-persist';
+import persistReducer from 'redux-persist/es/persistReducer';
+import storage from 'redux-persist/lib/storage';
+import { encryptTransform } from 'redux-persist-transform-encrypt';
+
 import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './features/authSlice';
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
+const encryptor = encryptTransform({
+  secretKey: import.meta.env.VITE_ENCRYPT_KEY,
+  onError: (error) => {
+    console.error('Error while encrypting', error);
   },
 });
+
+export const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'],
+  transforms: [encryptor],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };

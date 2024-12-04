be3683205ec69464e1c4eb942d3a64bb3503104f
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-04 07:50:25 +0700

    feat(front-components): define TrustBannerSection structure

---
+++ src/components/front/TrustBannerSection.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Typography } from '@components/commons';
+
+const TrustBannerSection = () => {
+  return (
+    <section>
+      {/* Logo */}
+      <img alt="" src="" />
+
+      {/* Heading */}
+      <Typography variant="subHeading">
+        Grow Your Business. Build Great Products.
+      </Typography>
+
+      {/* CTA Button */}
+      <button>Book a free call</button>
+
+      {/* Hands Illustration */}
+      <img alt="" src="" />
+    </section>
+  );
+};
+
+export default TrustBannerSection;

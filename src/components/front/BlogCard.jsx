e977bbd69941b0df079c10141d3e13d07f8b74f5
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-04 08:11:40 +0700

    feat(front-components): define BlogCardSection structure

---
+++ src/components/front/BlogCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Typography } from '@components/commons';
+
+const BlogCard = () => {
+  return (
+    <div>
+      {/* Image */}
+      <img alt="" src="" />
+      {/* Title */}
+      <Typography variant="blogCardTitle">
+        Building a stronger brand with no-code tools
+      </Typography>
+    </div>
+  );
+};
+
+export default BlogCard;

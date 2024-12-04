52ebf0a6cbd37db13f127904c0048e8d705d1310
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-04 08:21:31 +0700

    feat(front-pages): define Article page structure

---
+++ src/pages/front/Article.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { Typography } from '@components/commons';
+
+const Article = () => {
+  return (
+    <article>
+      <img alt="" src="" />
+      <Typography variant="headerText">
+        Build a stronger brand with no-code tools
+      </Typography>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam
+        accusantium vero odit optio velit, esse error dolorem ea. Nemo quaerat
+        ab labore, expedita doloremque ad accusantium fugit molestias a, soluta
+        dolorem, incidunt porro iure fuga. Dolores eveniet eius assumenda id
+        temporibus, ut, voluptate quos nemo rerum veritatis voluptates
+        laudantium illum vero. Numquam eveniet maxime repellendus repudiandae
+        laboriosam tempora ullam facere hic soluta sapiente, nulla ipsa libero
+        aspernatur expedita quibusdam quam quaerat officia quisquam similique
+        doloremque, inventore quis reprehenderit beatae asperiores. Excepturi
+        iusto quam odit possimus aliquam eum, temporibus quis dolor, quasi
+        dolore corrupti deleniti voluptatem voluptatibus. Expedita tempora
+        doloribus error qui repellendus quaerat fuga quos saepe quisquam quam.
+        Labore quis quas tenetur modi tempore, illum quia facere laudantium illo
+        id doloribus voluptate, nobis voluptatibus. Neque sequi nisi, ratione
+        aliquam animi quibusdam labore dolore dolorem, dicta temporibus totam ex
+        repudiandae incidunt optio nobis voluptates accusamus cupiditate magni
+        non earum, suscipit nihil qui consequatur! Quisquam hic exercitationem
+        aperiam illo laborum architecto harum accusantium corporis cumque
+        assumenda aliquid maxime cupiditate, voluptatibus laudantium unde sequi
+        adipisci aliquam dolore quos odio eaque fugit sint. Dicta, delectus
+        expedita porro perspiciatis doloribus deleniti animi impedit quod
+        laborum rerum deserunt nesciunt recusandae reprehenderit, magni iste
+        repellat, nihil dolorem?
+      </p>
+    </article>
+  );
+};
+
+export default Article;

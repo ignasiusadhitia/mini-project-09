f2ac5901459995262185409edabb139125febeca
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-04 11:26:28 +0700

    fix: set authSlice back to previous version

--- src/store/features/authSlice.js
+++ src/store/features/authSlice.js
@@ -1,42 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit';
-
 const initialState = {
   isAuthenticated: false,
-  token: null,
-  expiresAt: null,
-  rememberMe: false,
-  loading: false,
-  error: null,
-  isSuccess: false,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    login: (state, action) => {
+    login: (state) => {
       state.isAuthenticated = true;
-      state.token = action.payload.token;
-      state.expiresAt = action.payload.expiresAt;
-      state.rememberMe = action.payload.rememberMe;
-      state.loading = false;
-      state.error = null;
-      state.isSuccess = true;
     },
     logout: (state) => {
       state.isAuthenticated = false;
-      state.token = null;
-      state.expiresAt = null;
-      state.rememberMe = false;
-      state.loading = false;
-      state.error = null;
-      state.isSuccess = false;
     },
-  },
-  extraReducers: (builder) => {
-    builder.addCase();
+    register: (action) => {
+      console.log('User registered successfully', action.payload);
+    },
   },
 });
 
-export const { login, logout } = authSlice.actions;
+export const { login, logout, register } = authSlice.actions;
 export default authSlice.reducer;

ae3496d23346b1bc02fb01d7f9190c83708049d2
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-04 07:57:55 +0700

    feat(front-components): define PortfolioCard structure

---
+++ src/components/front/PortfolioCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PortfolioCard = () => {
+  return (
+    <div>
+      {/* Portfolio Image */}
+      <img alt="" src="" />
+      {/* Portfolio Title */}
+      <span>PortfolioTitle</span>
+    </div>
+  );
+};
+
+export default PortfolioCard;

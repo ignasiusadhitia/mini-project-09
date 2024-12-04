3cd4184bf9a402bf825dee15073b6578d05eed55
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-04 08:00:15 +0700

    feat(front-components): define Header  structure

---
+++ src/components/front/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Typography } from '@components/commons';
+
+const Header = () => {
+  return (
+    <header>
+      <Typography variant="headerText">HeaderText</Typography>
+      {/* Illustration */}
+      <img alt="" src="" />
+    </header>
+  );
+};
+
+export default Header;

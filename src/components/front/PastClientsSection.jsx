a60d361be35f052199c047e465e2e2e870d07dca
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-04 07:53:31 +0700

    feat(front-components): define PastClientsSection structure

---
+++ src/components/front/PastClientsSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Typography } from '@components/commons';
+
+const PastClientsSection = () => {
+  return (
+    <section>
+      {/* Heading */}
+      <Typography variant="subHeading">
+        We worked with the world’s biggest brands and the most innovative
+        startups
+      </Typography>
+
+      {/* Clients */}
+      <img alt="" src="" />
+    </section>
+  );
+};
+
+export default PastClientsSection;

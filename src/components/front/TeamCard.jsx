29b9f0643ac831f74230605de08cabd7089f74c3
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-04 08:03:14 +0700

    feat(front-components): define TeamCard structure

---
+++ src/components/front/TeamCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const TeamCard = () => {
+  return (
+    <div>
+      {/* Image */}
+      <img alt="" src="" />
+
+      {/* Name */}
+      <span>My Name</span>
+
+      {/* title */}
+      <span>CEO</span>
+
+      {/* Social Links */}
+      <div>
+        <a href="">Linkedin</a>
+        <a href="">Instagram</a>
+      </div>
+    </div>
+  );
+};
+
+export default TeamCard;

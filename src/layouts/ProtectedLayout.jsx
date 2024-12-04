dfa0b41ed71eae10be2ecbb8176c69b3c7f89cfb
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-05 00:36:10 +0700

    fix: protectedlayout header component

--- src/layouts/ProtectedLayout.jsx
+++ src/layouts/ProtectedLayout.jsx
@@ -29,7 +29,7 @@ const ProtectedLayout = ({ isAuthenticated }) => {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+        <header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator className="mr-2 h-4" orientation="vertical" />
           <Breadcrumb>

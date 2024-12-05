9800fb518a1e584087998c9a9502df8a7d6e5ec8
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-06 06:21:11 +0700

    feat(testimonial-dash): add testimonials pge

--- src/pages/dashboard/dashboard/Testimonials.jsx
+++ src/pages/dashboard/dashboard/Testimonials.jsx
@@ -1,7 +1,260 @@
-import React from 'react';
+/* eslint-disable */
+import { useState } from 'react';
+
+import { ArrowUpDown, MoreHorizontal } from 'lucide-react';
+
+import { DeleteConfirmation } from '@/components/dashboard';
+import { Alert } from '@/components/ui/alert';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Input } from '@/components/ui/input';
+import { Skeleton } from '@/components/ui/skeleton';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import testimonialsServices from '@/services/testimonialsServices';
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import useSWR from 'swr';
+
+// Column definitions
+const columns = [
+  {
+    accessorKey: 'foto_profile',
+    header: 'Profile Photo',
+    cell: ({ row }) => (
+      <img
+        className="h-8 w-8 rounded-full"
+        src={row.getValue('foto_profile')}
+        alt="testimonial-photo"
+      />
+    ),
+  },
+  {
+    accessorKey: 'name',
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+      >
+        Name
+        <ArrowUpDown />
+      </Button>
+    ),
+    cell: ({ row }) => <div className="capitalize">{row.getValue('name')}</div>,
+  },
+  {
+    accessorKey: 'title',
+    header: 'Title',
+    cell: ({ row }) => <div className="lowercase">{row.getValue('title')}</div>,
+  },
+  {
+    accessorKey: 'message',
+    header: 'Message',
+    cell: ({ row }) => (
+      <div className="lowercase">{row.getValue('message')}</div>
+    ),
+  },
+  {
+    id: 'actions',
+    enableHiding: false,
+    cell: ({ row }) => {
+      const testimonialId = row.original.id;
+      const navigate = useNavigate();
+
+      const handleEditTestimonial = (id) => {
+        navigate(`/dashboard/testimonials/edit/${id}`);
+      };
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="h-8 w-8 p-0" variant="ghost">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem className="cursor-pointer">
+              <DeleteConfirmation
+                id={testimonialId}
+                mutateKey="/blogs"
+                deleteHandler={testimonialsServices.deleteTestimonialById}
+                entityName="article"
+              />
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer">
+              <span onClick={() => handleEditTestimonial(testimonialId)}>
+                Edit
+              </span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  const [sorting, setSorting] = useState([]);
+  const [columnFilters, setColumnFilters] = useState([]);
+  const [columnVisibility, setColumnVisibility] = useState({});
+  const [rowSelection, setRowSelection] = useState({});
+  const navigate = useNavigate();
+  const { token } = useSelector((state) => state.auth);
+  const {
+    data: testimonials,
+    error,
+    isLoading,
+  } = useSWR(token ? '/testimonial' : null, () =>
+    testimonialsServices.fetchAllTestimonials(token)
+  );
+  const handleAddNewTestimonial = () => {
+    navigate('/dashboard/testimonial/add');
+  };
+
+  const tableData = testimonials?.data || [];
+
+  const table = useReactTable({
+    data: tableData,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  return (
+    <div className="w-full">
+      <div className="flex items-center justify-between py-4">
+        <Input
+          className="max-w-sm"
+          placeholder="Filter name..."
+          value={table.getColumn('name')?.getFilterValue() ?? ''}
+          onChange={(event) =>
+            table.getColumn('name')?.setFilterValue(event.target.value)
+          }
+        />
+        <Button onClick={handleAddNewTestimonial}>Add Testimonial</Button>
+      </div>
+
+      {isLoading && (
+        <div>
+          <Skeleton className="mb-2 h-10 w-full" />
+          <Skeleton className="h-10 w-full" />
+        </div>
+      )}
+
+      {error && (
+        <Alert variant="destructive" className="mb-4">
+          <p>Error loading users: {error.message}</p>
+        </Alert>
+      )}
+
+      {!isLoading && !error && (
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  ))}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {isLoading && <p>Loading...</p>}
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && 'selected'}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    className="h-24 text-center"
+                    colSpan={columns.length}
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      )}
+
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="space-x-2">
+          <Button
+            disabled={!table.getCanPreviousPage()}
+            size="sm"
+            variant="outline"
+            onClick={() => table.previousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            disabled={!table.getCanNextPage()}
+            size="sm"
+            variant="outline"
+            onClick={() => table.nextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
